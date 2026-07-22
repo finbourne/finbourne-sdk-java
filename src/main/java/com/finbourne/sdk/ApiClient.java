e89a94f668589821472db13abc557b6c96c0dcfc
Concourse <concourse@finbourne.com>
2026-07-22 14:29:29 +0000

    Java updates for 0.0.28
    Signed-off-by: Concourse <concourse@finbourne.com>

--- src/main/java/com/finbourne/sdk/ApiClient.java
+++ src/main/java/com/finbourne/sdk/ApiClient.java
@@ -111,7 +111,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/0.0.27/java");
+        setUserAgent("OpenAPI-Generator/0.0.28/java");
 
         authentications = new HashMap<String, Authentication>();
     }
